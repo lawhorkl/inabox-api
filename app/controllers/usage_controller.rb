de0454e352515bab8188e2f4980fac1351d1b364
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2017-11-27 13:29:52 -0500

    add stats to api

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -4,11 +4,23 @@ class UsageController < ApplicationController
     usage_stats = Vmstat.snapshot
     free_memory = usage_stats.memory.free_bytes
     ram_capacity = usage_stats.memory.total_bytes
+    disk_count = usage_stats.disks.size
+    filesystem = usage_stats.disks[0].type
+    block_size = usage_stats.disks[0].block_size
+    storage_capacity = usage_stats.disks[0].total_bytes
+    free_storage = usage_stats.disks[0].free_bytes
+    mount_point = usage_stats.disks[0].mount
     res = {
       :ram_capacity => ram_capacity,
       :free_ram => free_memory,
       :current_ram_usage => ram_capacity - free_memory,
       :cores_available => usage_stats.cpus.length,
+      :disk_count => disk_count,
+      :filesystem => filesystem,
+      :storage_capacity => storage_capacity,
+      :free_storage => free_storage,
+      :block_size => block_size,
+      :mount_point => mount_point,
       :active => true
     }
     render json: res, status: :ok
